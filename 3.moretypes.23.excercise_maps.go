72c650965c78d6055a63efac4dfceebb02dfea08
stepga <stepga@nirgendwo.eu>
2024-07-09 12:13:05 +0200

    moretypes 23

--- 3.moretypes.23.excercise_maps.go
+++ 3.moretypes.23.excercise_maps.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"golang.org/x/tour/wc"
+	"strings"
 )
 
+// XXX: go get golang.org/x/tour/wc
+
 func WordCount(s string) map[string]int {
-	// TODO: Implement WordCount. It should return a map of the counts of each
+	// DONE: Implement WordCount. It should return a map of the counts of each
 	// “word” in the string s. The wc.Test function runs a test suite against the
 	// provided function and prints success or failure.
 	// You might find strings.Fields helpful.
-	return map[string]int{"x": 1}
+	ret := make(map[string]int)
+	for _, key := range strings.Fields(s) {
+		v, ok := ret[key]
+		if ok {
+			ret[key] = v + 1
+		} else {
+			ret[key] = 1
+		}
+	}
+	return ret
 }
 
 func main() {
