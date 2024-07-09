8b12a4bb424d13f19b9a280088070a905c77c175
stepga <stepga@nirgendwo.eu>
2024-07-09 11:37:17 +0200

    moretypes 18

--- 3.moretypes.18.excercise_slices.go
+++ 3.moretypes.18.excercise_slices.go
@@ -1,11 +1,13 @@
 package main
 
+// XXX: go run 3.moretypes.18.excercise_slices.go | sed 's/IMAGE://g' | base64 -d  > foo; eog foo && rm foo
+
 import "golang.org/x/tour/pic"
 
 // XXX: go get golang.org/x/tour/pic
 
 func Pic(dx, dy int) [][]uint8 {
-	// TODO:  Implement Pic. It should return a slice of length dy, each element
+	// DONE: Implement Pic. It should return a slice of length dy, each element
 	// of which is a slice of dx 8-bit unsigned integers. When you run the
 	// program, it will display your picture, interpreting the integers as
 	// grayscale (well, bluescale) values.
@@ -13,6 +15,18 @@ func Pic(dx, dy int) [][]uint8 {
 	// x*y, and x^y.
 	// (You need to use a loop to allocate each []uint8 inside the [][]uint8.)
 	// (Use uint8(intValue) to convert between types.)
+	//ret := make([][]uint8, dy)
+	//return ret
+	var counter uint8 = 0
+	ret := make([][]uint8, dy)
+	for i, _ := range ret {
+		ret[i] = make([]uint8, dx)
+		for j, _ := range ret[i] {
+			ret[i][j] = counter
+			counter++
+		}
+	}
+	return ret
 }
 
 func main() {
