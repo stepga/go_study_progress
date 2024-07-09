23ca4e8ec6aac32ebef1a402f5b03befcf204222
stepga <stepga@nirgendwo.eu>
2024-07-09 14:03:25 +0200

    moretypes until 26

---
+++ 3.moretypes.26.exercise_fibonacci_closure.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// TODO: fibonacci is a function that returns a function that returns an int.
+func fibonacci() func() int {
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
