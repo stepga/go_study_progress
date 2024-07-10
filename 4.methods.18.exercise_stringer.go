42b628251e01b5f0fb3f14715cc7616bee6a98c1
stepga <stepga@nirgendwo.eu>
2024-07-10 10:44:05 +0200

    methods 18

--- 4.methods.18.exercise_stringer.go
+++ 4.methods.18.exercise_stringer.go
@@ -4,13 +4,18 @@ import "fmt"
 
 type IPAddr [4]byte
 
-// TODO: Add a "String() string" method to IPAddr.
+// Done: Add a "String() string" method to IPAddr.
+
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
+}
 
 func main() {
 	hosts := map[string]IPAddr{
 		"loopback":  {127, 0, 0, 1},
 		"googleDNS": {8, 8, 8, 8},
 	}
+	fmt.Println(IPAddr{4, 4, 4, 4})
 	for name, ip := range hosts {
 		fmt.Printf("%v: %v\n", name, ip)
 	}
