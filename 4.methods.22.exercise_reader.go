7baadbd7393ba939a2e8e716e50d79074532f97f
stepga <stepga@nirgendwo.eu>
2024-07-10 12:15:31 +0200

    methods 22

--- 4.methods.22.exercise_reader.go
+++ 4.methods.22.exercise_reader.go
@@ -4,7 +4,17 @@ import "golang.org/x/tour/reader"
 
 type MyReader struct{}
 
-// TODO: Add a Read([]byte) (int, error) method to MyReader.
+// DONE: Add a Read([]byte) (int, error) method to MyReader.
+
+func (m MyReader) Read(b []byte) (int, error) {
+	b[0] = byte('A')
+	return 1, nil
+	// XXX or:
+	//for x := range b {
+	//	b[x] = 'A'
+	//}
+	//return len(b), nil
+}
 
 func main() {
 	reader.Validate(MyReader{})
