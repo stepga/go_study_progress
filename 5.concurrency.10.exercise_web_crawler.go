0f4c633d2cefab16387778fb6de09f001da1d922
stepga <stepga@nirgendwo.eu>
2024-07-11 13:36:41 +0200

    concurrency 10 (with the help of waitgroups)

--- 5.concurrency.10.exercise_web_crawler.go
+++ 5.concurrency.10.exercise_web_crawler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 type Fetcher interface {
@@ -10,29 +11,71 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+type SafeMap struct {
+	mutex      sync.Mutex
+	string_map map[string]string
+}
+
+func (safe_map *SafeMap) SetUnsetKey(key string, val string) {
+	safe_map.mutex.Lock()
+	_, ok := safe_map.string_map[key]
+	if !ok {
+		safe_map.string_map[key] = val
+	}
+	safe_map.mutex.Unlock()
+}
+
+func (safe_map *SafeMap) KeyExists(key string) bool {
+	safe_map.mutex.Lock()
+	defer safe_map.mutex.Unlock()
+	_, ok := safe_map.string_map[key]
+	return ok
+
+}
+
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: Fetch URLs in parallel.
-	// TODO: Don't fetch the same URL twice.
-	// This implementation doesn't do either:
-	if depth <= 0 {
+func Crawl(url string, depth int, fetcher Fetcher) map[string]string {
+	var safe_map = SafeMap{string_map: make(map[string]string)}
+	var wait_group sync.WaitGroup
+	var crawler func(url string, depth int, fetcher Fetcher)
+
+	crawler = func(url string, depth int, fetcher Fetcher) {
+		defer wait_group.Done()
+
+		if depth <= 0 {
+			return
+		}
+		if safe_map.KeyExists(url) {
+			return
+		}
+
+		body, urls, err := fetcher.Fetch(url)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		safe_map.SetUnsetKey(url, body)
+		for _, u := range urls {
+			wait_group.Add(1)
+			go crawler(u, depth-1, fetcher)
+		}
 		return
 	}
-	body, urls, err := fetcher.Fetch(url)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("found: %s %q\n", url, body)
-	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
-	}
-	return
+
+	wait_group.Add(1)
+	go crawler(url, depth, fetcher)
+	wait_group.Wait()
+
+	return safe_map.string_map
 }
 
 func main() {
-	Crawl("https://golang.org/", 4, fetcher)
+	ret := Crawl("https://golang.org/", 4, fetcher)
+
+	for url, body := range ret {
+		fmt.Printf("found: %s %q\n", url, body)
+	}
 }
 
 // fakeFetcher is Fetcher that returns canned results.
